c98deb6a2dbc8535078314b8d422283bf017b2f4
yuriy <yury.kvak@leroymerlin.ru>
2021-05-19 20:55:30 +0300

    Homework from lesson 1

---
+++ Java_Level2/lesson1/src/Cat.java
@@ -0,0 +1,41 @@
+public class Cat implements Trackable{
+    private int height;
+    private int distance;
+    private boolean out;
+
+    public Cat(int height, int distance){
+        this.height = height;
+        this.distance = distance;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void jump(int height){
+        System.out.println("Cat started jumping");
+        out = height > this.height;
+        if (!out)
+            System.out.println("Cat jumped over");
+        else
+            System.out.println("Cat did not jump over and will not be allowed to next task");
+    }
+
+    public void run(int distance){
+        System.out.println("Cat started running");
+        out = distance > this.distance;
+        if (!out)
+            System.out.println("Cat finished distance");
+        else
+            System.out.println("Cat did not finish distance and will not be allowed to next task");
+    }
+
+    @Override
+    public boolean isOutOfGame() {
+        return out;
+    }
+}
---
+++ Java_Level2/lesson1/src/Finishable.java
@@ -0,0 +1,3 @@
+public interface Finishable {
+    void passOver(Trackable participant);
+}
