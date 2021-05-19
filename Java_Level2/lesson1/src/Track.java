5545b70d4acb659f80f88b3c93f3e411e46e5bdf
yuriy <yury.kvak@leroymerlin.ru>
2021-05-19 21:10:45 +0300

    Homework from lesson 1

---
+++ Java_Level2/lesson1/src/Track.java
@@ -0,0 +1,17 @@
+/**
+ * Class track
+ */
+public class Track implements Finishable{
+    private int distance;
+
+    public Track(int distance){
+        this.distance = distance;
+    }
+
+    public void passOver(Trackable participant){
+        participant.run(this.distance);
+    }
+
+
+
+}
